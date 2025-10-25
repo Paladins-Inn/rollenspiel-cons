53d8b9440a9f20bf0c5fa9eedd3a1c21df8dcb30
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2025-10-25 23:09:03 +0200

    feat(web-ui): Moved page creation to the GlobalModelAttributes provider and removing the now unneeded AbstractBaseController.

--- src/main/java/de/paladinsinn/rollenspielcons/controllers/LegaleseController.java
+++ src/main/java/de/paladinsinn/rollenspielcons/controllers/LegaleseController.java
@@ -12,14 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/legalese")
 @XSlf4j
-public class LegaleseController extends AbstractBaseController {
+public class LegaleseController {
   
   @PermitAll
   @GetMapping("/about")
   public String about(@AuthenticationPrincipal OAuth2User user, Model model) {
     log.entry(user, model);
     
-    return forwarder(user, model, "pages/legalese/about");
+    return "pages/legalese/about";
   }
   
   
@@ -28,7 +28,7 @@ public class LegaleseController extends AbstractBaseController {
   public String tos(@AuthenticationPrincipal OAuth2User user, Model model) {
     log.entry(user, model);
     
-    return forwarder(user, model, "pages/legalese/tos");
+    return "pages/legalese/tos";
   }
   
   
@@ -37,7 +37,7 @@ public class LegaleseController extends AbstractBaseController {
   public String dataprotection(@AuthenticationPrincipal OAuth2User user, Model model) {
     log.entry(user, model);
     
-    return forwarder(user, model, "pages/legalese/dataprotection");
+    return "pages/legalese/dataprotection";
   }
   
   
@@ -46,7 +46,7 @@ public class LegaleseController extends AbstractBaseController {
   public String imprint(@AuthenticationPrincipal OAuth2User user, Model model) {
     log.entry(user, model);
     
-    return forwarder(user, model, "pages/legalese/imprint");
+    return "pages/legalese/imprint";
   }
   
   
