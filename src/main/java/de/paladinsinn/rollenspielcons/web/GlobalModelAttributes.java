2dcd68cb340f519d0e30d3bd55161c5d0c3f3ed7
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2025-10-25 23:00:24 +0200

    chore(web-ui): Moved contextPath to a GlobalModelAttributes provided value.

--- src/main/java/de/paladinsinn/rollenspielcons/web/GlobalModelAttributes.java
+++ src/main/java/de/paladinsinn/rollenspielcons/web/GlobalModelAttributes.java
@@ -3,6 +3,7 @@ package de.paladinsinn.rollenspielcons.web;
 import de.paladinsinn.rollenspielcons.services.markdown.MarkdownService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.XSlf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -12,10 +13,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class GlobalModelAttributes {
   private final MarkdownService markdownService;
 
+  @Value("${server.servlet.context-path:}")
+  private String contextPath;
   
   @ModelAttribute("md")
   public MarkdownService markdownService() {
-    return markdownService;
+    log.entry();
+    return log.exit(markdownService);
+  }
+  
+  @ModelAttribute("contextPath")
+  public String contextPath() {
+    log.entry();
+    return log.exit(contextPath);
   }
 }
 
